6c60608a51bf10db2a04005c0e985497b8dae304
Hans Park <hans.park6@gmail.com>
2023-01-12 02:02:41 -0500

    finished vm translator part 1, all tests passed

--- 07/vmtranslator/src/vmtranslator/Parser.java
+++ 07/vmtranslator/src/vmtranslator/Parser.java
@@ -1,6 +1,7 @@
 package vmtranslator;
 
 import java.io.*;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
@@ -10,18 +11,20 @@ and determines what methods to call from the CodeWriter class next.
 */
 public class Parser {
     // Static variables
-    public static final HashSet<String> ARITHMETIC = new HashSet<>();
-    public static final int TMPOFFSET = 5;
+    private static final HashSet<String> ARITHMETIC = new HashSet<>();
+    private static final HashMap<String, String> REG_MAP = new HashMap<>();
+    private static final int TMP_OFFSET = 5;
 
     // Instance variables
-    BufferedReader bufferedReader;
-    PrintWriter printWriter;
+    private final BufferedReader bufferedReader;
+    private final PrintWriter printWriter;
 
     private final String filePrefix;
     private String currInstruct;
     private String commandType;
     private String arg1;
     private String arg2;
+    private int jumpNum;
 
     // Constructor
     public Parser(String source) throws IOException {
@@ -35,8 +38,13 @@ public class Parser {
         FileWriter fileWriter = new FileWriter(filePrefix + ".asm");
         this.printWriter = new PrintWriter(new BufferedWriter(fileWriter));
 
-        // Add arithmetic commands to ARITHMETIC
         ARITHMETIC.addAll(List.of("add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"));
+        REG_MAP.put("local", "LCL");
+        REG_MAP.put("argument", "ARG");
+        REG_MAP.put("this", "THIS");
+        REG_MAP.put("that", "THAT");
+
+        this.jumpNum = 0;
     }
 
     /*
@@ -92,6 +100,12 @@ public class Parser {
             // Todo: add more commandType cases later
             this.advance();
         }
+        // put infinite loop at end of asm file
+        printWriter.println("// infinite loop");
+        printWriter.println("(END)");
+        printWriter.println("@END");
+        printWriter.println("0;JMP");
+
         bufferedReader.close();
         printWriter.close();
     }
@@ -103,9 +117,10 @@ public class Parser {
     */
     private void writeArithmetic() {
         String op;
+        String jumpName;
 
         if (ARITHMETIC.contains(arg1)) {
-            printWriter.println("//" + currInstruct);
+            printWriter.println("// " + currInstruct);
 
             if (List.of("add", "sub", "and", "or").contains(arg1)) {
                 printWriter.println("@SP");
@@ -118,36 +133,41 @@ public class Parser {
                     case "sub" -> "-";
                     case "and" -> "&";
                     case "or" -> "|";
+                    case default -> throw new IllegalArgumentException("Unexpected arithmetic op");
                 };
-                printWriter.println("M=D" + op + "M");
+                printWriter.println("M=M" + op + "D");
 
             } else if (List.of("not", "neg").contains(arg1)) {
                 printWriter.println("@SP");
                 printWriter.println("A=M-1");
                 op = switch (arg1) {
-                    case "not" -> "-";
-                    case "neg" -> "!";
+                    case "not" -> "!";
+                    case "neg" -> "-";
+                    case default -> throw new IllegalArgumentException("Unexpected negation op");
                 };
                 printWriter.println("M=" + op + "M");
 
             } else { // eq, gt, lt
+                jumpName = "EQ_jump" + jumpNum;
                 printWriter.println("@SP");
                 printWriter.println("AM=M-1");
                 printWriter.println("D=M");
                 printWriter.println("A=A-1");
                 printWriter.println("D=M-D");
                 printWriter.println("M=-1");
-                printWriter.println("@EQ_jump");
+                printWriter.println("@" + jumpName);
                 op = switch (arg1) {
                     case "eq" -> "JEQ";
                     case "gt" -> "JGT";
                     case "lt" -> "JLT";
+                    case default -> throw new IllegalArgumentException("Unexpected equality op");
                 };
                 printWriter.println("D;" + op);
                 printWriter.println("@SP");
                 printWriter.println("A=M-1");
                 printWriter.println("M=0");
-                printWriter.println("(@EQ_jump)");
+                printWriter.println("(" + jumpName + ")");
+                jumpNum += 1;
             }
         }
     }
@@ -163,7 +183,7 @@ public class Parser {
         if (!commandType.equals("pop") && !commandType.equals("push")) {
             return;
         }
-        printWriter.println("//" + currInstruct);
+        printWriter.println("// " + currInstruct);
 
         if (commandType.equals("push")) {
             switch (arg1) {
@@ -172,31 +192,33 @@ public class Parser {
                     printWriter.println("D=A");
                 }
                 case "pointer" -> {
-                    if (arg2.equals("0")) {
-                        op = "@THIS";
-                    } else {
-                        op = "@THAT";
-                    }
-                    printWriter.println(op);
-                    printWriter.println("@" + arg2);
-                    printWriter.println("D=A");
+                    op = switch (arg2) {
+                        case "0" -> "THIS";
+                        case "1" -> "THAT";
+                        case default -> throw new IllegalArgumentException("Unexpected pointer value");
+                    };
+                    printWriter.println("@" + op);
+                    printWriter.println("D=M");
                 }
                 case "temp" -> {
-                    printWriter.println("@R" + (Integer.parseInt(arg2) + TMPOFFSET));
+                    op = "R" + (Integer.parseInt(arg2) + TMP_OFFSET);
+                    printWriter.println("@" + op);
                     printWriter.println("D=M");
                 }
                 case "static" -> {
-                    printWriter.println("@" + filePrefix + arg2);
+                    op = filePrefix + "." + arg2;
+                    printWriter.println("@" + op);
                     printWriter.println("D=M");
                 }
                 default -> {  // argument, local, this, that
-                    printWriter.println("@" + arg1);
+                    printWriter.println("@" + REG_MAP.get(arg1));
                     printWriter.println("D=M");
                     printWriter.println("@" + arg2);
                     printWriter.println("A=D+A");
                     printWriter.println("D=M");
                 }
             }
+            // All push commands end with these 4 lines
             printWriter.println("@SP");
             printWriter.println("M=M+1");
             printWriter.println("A=M-1");
@@ -204,40 +226,43 @@ public class Parser {
 
         } else { // commandType.equals("pop")
             if (List.of("argument", "local", "this", "that").contains(arg1)) {
-                printWriter.println("@" + arg1);
+                printWriter.println("@" + REG_MAP.get(arg1));
                 printWriter.println("D=M");
                 printWriter.println("@" + arg2);
                 printWriter.println("D=D+A");
                 printWriter.println("@R13");
                 printWriter.println("M=D");
-                printWriter.println("@SP");
-                printWriter.println("AM=M-1");
-                printWriter.println("D=M");
+                printWriter.println("@SP");     // These 3 lines are in common with other cases.
+                printWriter.println("AM=M-1");  // but don't have good way to separate them
+                printWriter.println("D=M");     //
                 printWriter.println("@R13");
                 printWriter.println("A=M");
                 printWriter.println("M=D");
                 return;
             }
+            // Else case: pointer, temp, static
             printWriter.println("@SP");
             printWriter.println("AM=M-1");
             printWriter.println("D=M");
 
             switch (arg1) {
                 case "pointer" -> {
-                    if (arg2.equals("0")) {
-                        op = "@THIS";
-                    } else {
-                        op = "@THAT";
-                    }
-                    printWriter.println(op);
+                    op = switch (arg2) {
+                        case "0" -> "THIS";
+                        case "1" -> "THAT";
+                        case default -> throw new IllegalArgumentException("Unexpected pointer value");
+                    };
+                    printWriter.println("@" + op);
                     printWriter.println("M=D");
                 }
                 case "temp" -> {
-                    printWriter.println("@R" + (Integer.parseInt(arg2) + TMPOFFSET));
+                    op = "R" + (Integer.parseInt(arg2) + TMP_OFFSET);
+                    printWriter.println("@" + op);
                     printWriter.println("M=D");
                 }
                 case "static" -> {
-                    printWriter.println("@" + filePrefix + arg2);
+                    op = filePrefix + "." + arg2;
+                    printWriter.println("@" + op);
                     printWriter.println("M=D");
                 }
             }
