11d857ca4886f8eff69b258b31cd0edb14e31bb4
Hans Park <hans.park6@gmail.com>
2023-04-20 13:29:15 -0400

    about to fix small bug in tokenizer where it cannot handle empty file

--- 10/Compiler/src/CompilationEngine.java
+++ 10/Compiler/src/CompilationEngine.java
@@ -1,28 +1,31 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FilenameFilter;
-import java.util.ArrayDeque;
-import java.util.List;
-import java.util.Objects;
+import java.io.*;
 
 /* The CompilationEngine should grab tokens from the tokenizer one-by-one, analyze the grammar,
 and emit a structured representation of the source code in a xml file */
 public class CompilationEngine {
-    private final ArrayDeque<String> outputFiles = new ArrayDeque<>();
+    private Tokenizer tk;
+    private PrintWriter writer;
 
-    CompilationEngine(String source) {
-        /* Build the list of output files */
-        if (source.endsWith(".jack")) { // source is .jack file -> one xml file
-            String prefix = source.substring(0, source.length() - ".jack".length());
-            outputFiles.add(prefix + ".xml");
-        }
-        else { // source is file direction -> multiple xml files
-            File directoryPath = new File("./" + source);
-            FilenameFilter fileFilter = (dir, name) -> {
-                String lowercaseName = name.toLowerCase();
-                return lowercaseName.endsWith(".jack");
-            };
-            outputFiles.addAll(List.of(Objects.requireNonNull(directoryPath.list(fileFilter))));
-        }}
+
+    /* Build the list of output files */
+    CompilationEngine(String source) throws IOException {
+        tk = new Tokenizer(source);
+        String prefix = source.substring(0, source.length() - 5);
+        PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(prefix + ".xml")));
+    }
+
+    /** Analyze the grammar of the source file and output a structured representation to an XML file */
+    public void printToXML() throws IOException {
+        tk.advance();
+        Tokenizer.TokenType type = tk.getTokenType();
+
+
+    }
+    private void compile() throws IOException {
+        tk.advance();
+    }
+
+    private void compileClass() {
+
+    }
 }
--- 10/Compiler/src/Main.java
+++ 10/Compiler/src/Main.java
@@ -1,22 +1,30 @@
-
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Objects;
+
+import static java.lang.System.exit;
 
 public class Main {
-
-    // Main = syntax analyzer
     public static void main(String[] args) throws IOException, InterruptedException {
         if (args.length != 1) {
-            System.out.println("Must have only one argument.");
-        } else {
-            // source is either a filename or the name of a folder
-            Tokenizer tk = new Tokenizer(args[0]);
+            System.out.println("Must have only one argument that is either a .jack file or directory.");
+            exit(-1);
+        }
+        ArrayDeque<String> sourceFiles = new ArrayDeque<>();
+        if (args[0].endsWith(".jack")) { // If the argument is a .jack file, add it to the deque
+            sourceFiles.add(args[0]);
 
-            // arg[0] is passed to compilationengine, and arg[0] can either be a directory or jack file
-            //
-            // CompilationEngine determines whether arg is file or directory
-            // if directly, builds a list of output files
-
+        } else { // If the argument is a directory, add all .jack files in the directory to the deque
+            File directory = new File(args[0]);
+            FilenameFilter jackFilter = (dir, name) -> name.toLowerCase().endsWith(".jack");
+            sourceFiles.addAll(List.of(Objects.requireNonNull(directory.list(jackFilter))));
+        }
+        /* Iterate through sourceFiles and compile */
+        for (String file : sourceFiles) {
+            CompilationEngine compiler = new CompilationEngine(file);
         }
     }
-
 }
