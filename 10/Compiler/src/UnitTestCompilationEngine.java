bdc6b4673c668e06defa64be8efb7ca4ab02aa6f
Hans Park <hans.park6@gmail.com>
2023-04-22 17:18:04 -0400

    ExpressionLessSquare passes

--- 10/Compiler/src/UnitTestCompilationEngine.java
+++ 10/Compiler/src/UnitTestCompilationEngine.java
@@ -33,4 +33,14 @@ public class UnitTestCompilationEngine {
         CompilationEngine ce = new CompilationEngine("./src/TestFiles/ArrayTest/Main.jack");
         ce.compile();
     }
+
+    @Test
+    public void expressionlessSquareTest() throws IOException {
+        CompilationEngine ce1 = new CompilationEngine("./src/TestFiles/ExpressionLessSquare/Main.jack");
+        CompilationEngine ce2 = new CompilationEngine("./src/TestFiles/ExpressionLessSquare/Square.jack");
+        CompilationEngine ce3 = new CompilationEngine("./src/TestFiles/ExpressionLessSquare/SquareGame.jack");
+        ce1.compile();
+        ce2.compile();
+        ce3.compile();
+    }
 }
